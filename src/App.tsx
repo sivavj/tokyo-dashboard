d79da51a93fd60f6968c3cee67c6abff5cfc4161
Siva <sivarajendran@enthutech.in>
2022-05-20 15:57:47 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useRoutes } from 'react-router-dom';
+import routes from './router';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+
+import ThemeProvider from './theme/ThemeProvider';
+import { CssBaseline } from '@mui/material';
+import ThemeSettings from './ThemeSettings';
+
+const App = () => {
+
+  const content = useRoutes(routes);
+
+  return (
+    <ThemeProvider>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <CssBaseline />
+        {content}
+        <ThemeSettings/>
+      </LocalizationProvider>
+    </ThemeProvider>
+  );
+}
+export default App;
